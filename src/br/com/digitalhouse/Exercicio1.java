e681925be98d1c8f1a77209f0827fc414524abfd
unknown <Digital@DH-A8-30.dhbrasil.digitalhouse.com>
2019-04-03 22:35:33 -0300

    Initial commit

---
+++ src/br/com/digitalhouse/Exercicio1.java
@@ -0,0 +1,28 @@
+package br.com.digitalhouse;
+
+import jdk.nashorn.internal.ir.ReturnNode;
+import sun.font.TrueTypeFont;
+
+public class Exercicio1 {
+    public static void main(String[] args) {
+        Integer umNumeroA;
+        Integer umNumeroB;
+
+        umNumeroA = 10;
+        umNumeroB = 20;
+
+        if (umNumeroA < umNumeroB){
+            System.out.println(true);
+
+        }else {
+            System.out.println(false);
+
+        }
+
+
+
+
+
+
+    }
+}
---
+++ src/br/com/digitalhouse/Exercicio2.java
@@ -0,0 +1,9 @@
+package br.com.digitalhouse;
+
+public class Exercicio2 {
+    public static void main(String[] args) {
+        Integer umNumero;
+
+        umNumero = 15
+    }
+}
